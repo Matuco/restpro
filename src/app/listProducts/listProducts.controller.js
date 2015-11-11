02570f4b0e91f8dc5f6d8f92479ed0cef6a380cf
Matuco <matias.bravo@prosmartsoluciones.cl>
2015-11-11 20:44:08 -0300

    Fixed ListProducts btns menu

--- src/app/listProducts/listProducts.controller.js
+++ src/app/listProducts/listProducts.controller.js
@@ -7,16 +7,22 @@
     /** @ngInject */
     function ListProductsController($routeParams, dataFactoryPlatos) {
         var vm = this;        
-        vm.categoria = $routeParams.categoria; //Categoría 
+        vm.categoria = $routeParams.categoria; //CategorÃ­a
+        vm.categoriacorto = "";
         vm.platos = [];
 
         dataFactoryPlatos.getPlatosCategoria(vm.categoria)
         .then(function (response) {
             vm.platos = response.data;
             console.log("Entro a Factory Service. Categoria : " + vm.categoria + '(' + response.data.length + ')');
-            console.log();
+            if (vm.platos.length > 0) {
+            vm.categoriacorto = vm.platos[0].CategoriaCorto;
+            console.log("Categoria Corto : " + vm.categoriacorto);
+        };
         }, function (error) {
             vm.platos = error || "Llamada a la API para Platos por categoria fallo!";
         });
+        
+        
     }
 })();
